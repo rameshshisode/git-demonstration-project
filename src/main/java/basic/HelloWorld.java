b4195fed565a2e799fb99a48becbd3cf50975b46
Ramesh S <rameshshisode781@gmail.com>
2025-09-12 21:13:38 +0530

    initial commit

---
+++ src/main/java/basic/HelloWorld.java
@@ -0,0 +1,14 @@
+package basic;
+
+public class HelloWorld {
+	
+	public void welcomeMsg() {
+		System.out.println("Weclcome to Java Programming!");
+	}
+
+	public static void main(String[] args) {
+		HelloWorld obj = new HelloWorld();
+		obj.welcomeMsg();
+	}
+
+}
