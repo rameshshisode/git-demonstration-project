e9b7b2422412498b9ecc2d697082b7a5901ed23c
Ramesh S <rameshshisode781@gmail.com>
2025-09-15 21:52:29 +0530

    added new file and modifyed the exsiting file

---
+++ src/main/java/basic/student.java
@@ -0,0 +1,16 @@
+package basic;
+//demo of branching and PR flow
+public class student {
+	
+	public String studentName;
+	public int studentAge;
+	public String studentEmail;
+	
+	public void displayStudentInfo() {
+		System.out.println("Studen Name: " + studentName);
+		System.out.println("Studen Age: " + studentAge);
+		System.out.println("Studen Email: " + studentEmail);
+		
+	}
+
+}
